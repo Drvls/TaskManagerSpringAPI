95ccb8be0c559b8f306a3949f2a63c878eebb561
Drvls <mestreunder@gmail.com>
2026-05-17 13:11:44 -0300

    feat: add global exception handler with TaskNotFoundException

---
+++ src/main/java/org/alexvsi/taskmanager/infra/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package org.alexvsi.taskmanager.infra.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+}
---
+++ src/main/java/org/alexvsi/taskmanager/infra/exception/TaskNotFoundException.java
@@ -0,0 +1,7 @@
+package org.alexvsi.taskmanager.infra.exception;
+
+public class TaskNotFoundException extends RuntimeException{
+    public TaskNotFoundException(String message){
+        super(message);
+    }
+}
