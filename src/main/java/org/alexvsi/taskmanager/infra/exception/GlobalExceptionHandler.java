59cafb751d1f6dcba1d8aa921020a620a5fd8c06
Drvls <mestreunder@gmail.com>
2026-05-17 17:25:17 -0300

    feat: add validation exception handler

--- src/main/java/org/alexvsi/taskmanager/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/org/alexvsi/taskmanager/infra/exception/GlobalExceptionHandler.java
@@ -1,9 +1,16 @@
 package org.alexvsi.taskmanager.infra.exception;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +19,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, @Nullable String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getBindingResult().getFieldErrors().stream()
+                        .collect(Collectors.toMap(
+                                FieldError::getField,
+                                DefaultMessageSourceResolvable::getDefaultMessage)));
     }
 }
